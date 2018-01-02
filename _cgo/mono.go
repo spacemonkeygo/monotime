6bf3a07804e79951ca1b7aca5f8eb29d384826d4
Jeff Wendling <jeff@spacemonkey.com>
2018-01-02 14:13:36 -0700

    remove allocation from cgo path

--- _cgo/mono.go
+++ _cgo/mono.go
@@ -16,45 +16,17 @@ package _cgo
 
 /*
 #include <time.h>
-#include <stdint.h>
 
-#ifdef __MACH__
-#include <mach/mach.h>
-#include <mach/mach_time.h>
-#endif
-
-int nano_count(struct timespec *ts) {
-#ifdef __MACH__
-  static mach_timebase_info_data_t info;
-  if (info.denom == 0) {
-    mach_timebase_info(&info);
-  }
-  uint64_t something = mach_absolute_time();
-  something /= info.denom;
-  something *= info.numer;
-  ts->tv_sec = something / 1000000000;
-  ts->tv_nsec = something % 1000000000;
-  return 0;
-#else
-#ifdef CLOCK_MONOTONIC_RAW
-  if (clock_gettime(CLOCK_MONOTONIC_RAW, ts) < 0) {
-#endif
-  if (clock_gettime(CLOCK_MONOTONIC, ts) < 0) {
-    return -1;
-  }
-#ifdef CLOCK_MONOTONIC_RAW
-  }
-#endif
-#endif
-  return 0;
-}
+struct timespec nano_count();
 */
 import "C"
 
+//export cpanic
+func cpanic() {
+	panic("failed getting monotonic clock")
+}
+
 func Monotime() (sec int64, nsec int32) {
-	var rv C.struct_timespec
-	if C.nano_count(&rv) != 0 {
-		panic("failed getting monotonic clock")
-	}
+	rv := C.nano_count()
 	return int64(rv.tv_sec), int32(rv.tv_nsec)
 }
